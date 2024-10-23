2d9fca41851055fd317ab76268f2cdd034daccd0
SaltyChocolat <rexrares009@gmail.com>
2024-10-23 19:58:01 +0300

    Added Keyboard Functionality + Design Changes

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let x =null;
 let  y =null;
 let op;
 const nums =["0","1","2","3","4","5","6","7","8","9","."]
-const operators =["+","-","x","/","=","%","+/-","AC"]
+const operators =["+","-","x","/","=","%","+/-","AC","DEL"]
 
 const buttons = document.querySelector(".buttons")
 const topDisplay = document.querySelector("#top")
@@ -47,90 +47,128 @@ function operate(x,y,op){
             return rest(x,y);
     }
 }
-buttons.addEventListener("click",function(event){
-    target = event.target
-    if(target.className.includes("box")){
+
+function inputValue(value){
+    if(nums.includes(value)){
         clickSound.play()
-        value =  target.textContent
-        if(nums.includes(value)){
-            if(value=="."){
-                if(x!=null && op==null){
-                    if(!(botDisplay.textContent.includes("."))){
-                        x+="."
-                        botDisplay.textContent=x;
-                    }
-                }
-                else if(y!=null){
-                    if(!(botDisplay.textContent.includes("."))){
-                    y+="."
-                    botDisplay.textContent=y;
-                    }
+        if(value=="."){
+            if(x!=null && op==null){
+                if(!(botDisplay.textContent.includes("."))){
+                    x+="."
+                    botDisplay.textContent=x;
                 }
             }
-            else if(x==null){
-                x=value
-                botDisplay.textContent=x;
-            }
-            else if(x!=null && op==null){
-                x+=value
-                botDisplay.textContent=x;
-            }
-            else if(y==null){
-                y=value
+            else if(y!=null){
+                if(!(botDisplay.textContent.includes("."))){
+                y+="."
                 botDisplay.textContent=y;
+                }
+            }
+        }
+        else if(x==null){
+            x=value
+            botDisplay.textContent=x;
+        }
+        else if(x!=null && op==null){
+            if(x!="0")
+                x+=value
+            else
+                x=value
+            botDisplay.textContent=x;
+        }
+        else if(y==null){
+            y=value
+            botDisplay.textContent=y;
+        }
+        else{
+            if(y!="0")
+                y+=value
+            else
+                y=value
+            botDisplay.textContent=y;
+        }
+    }
+    if(operators.includes(value)){
+        clickSound.play()
+        if(value=="AC"){
+            botDisplay.textContent="";
+            topDisplay.textContent=""
+            x=null;
+            y=null;
+            op=null;
+        }
+        else if(value=="+/-"){
+            if(x!=null && y==null){
+                x=-x;
+                botDisplay.textContent=x;
             }
             else{
-                y+=value
+                y=-y;
                 botDisplay.textContent=y;
             }
         }
-        if(operators.includes(value)){
-            if(value=="AC"){
-                botDisplay.textContent="";
-                topDisplay.textContent=""
-                x=null;
+        else if(value=="DEL"){
+            if(x!=null && y==null){
+                x = x.slice(0,-1);
+                botDisplay.textContent =x;
+            }
+            else if(x!=null && y!=null){
+                y=y.slice(0,-1)
+                topDisplay.textContent = y;
+            }
+        }
+        else if(value=="="){
+            if(x!=null && y!=null && op!=null){
+                x = String(operate(x,y,op));
+                botDisplay.textContent=x;
+                topDisplay.textContent="";
+                midDisplay.textContent="";
                 y=null;
                 op=null;
             }
-            else if(value=="+/-"){
-                if(x!=null && y==null){
-                    x=-x;
-                    botDisplay.textContent=x;
-                }
-                else{
-                    y=-y;
-                    botDisplay.textContent=y;
-                }
-            }
-            else if(value=="="){
-                if(x!=null && y!=null && op!=null){
-                    x = String(operate(x,y,op));
-                    botDisplay.textContent=x;
-                    topDisplay.textContent="";
-                    midDisplay.textContent="";
-                    y=null;
-                    op=null;
-                }
 
+        }
+        else{
+            if(x!=null && y!=null && op!=null){
+                x = String(operate(x,y,op));
+                botDisplay.textContent="";
+                topDisplay.textContent=x;
+                midDisplay.textContent=op;
+                y=null;
+                op=null;
             }
-            else{
-                if(x!=null && y!=null && op!=null){
-                    x = String(operate(x,y,op));
-                    botDisplay.textContent="";
-                    topDisplay.textContent=x;
-                    midDisplay.textContent=op;
-                    y=null;
-                    op=null;
-                }
-                else if(x!=null){
-                    topDisplay.textContent=x;
-                    botDisplay.textContent="";  
-                }
-                op=value;
-                if(!(x==null && y==null))
-                    midDisplay.textContent=op;
+            else if(x!=null){
+                topDisplay.textContent=x;
+                botDisplay.textContent="";  
+            }
+            op=value;
+            if(!(x==null && y==null))
+                midDisplay.textContent=op;
 
-            }
         }
     }
+
+}
+buttons.addEventListener("click",function(event){
+    target = event.target
+    if(target.className.includes("box")){
+        value =  target.textContent
+        inputValue(value);
+    }
+})
+
+
+document.addEventListener("keyup",function(event){
+    key = event.key
+    switch(key){
+        case "Enter":
+            inputValue("=")
+            return
+        case "Backspace":
+            inputValue("DEL")
+            return 
+        default:
+            inputValue(key)  
+        
+    }      
 })
